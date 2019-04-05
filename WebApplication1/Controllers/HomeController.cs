a3775810ffdb2bd57af4775c09c67a76bffe8dd8
Mindaugas Pikelis <36482664+justcallmeM@users.noreply.github.com>
2019-04-05 20:00:44 +0300

    Added Developement branch Added additional html windows for laboratories Home controller now hosts all laboratory windows.

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -10,14 +10,30 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Lab1()
         {
+            ViewData["Message"] = "Description of laboratory 1";
+
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Lab2()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Description of laboratory 2";
+
+            return View();
+        }
+
+        public IActionResult Lab3()
+        {
+            ViewData["Message"] = "Description of laboratory 3";
+
+            return View();
+        }
+
+        public IActionResult Lab4()
+        {
+            ViewData["Message"] = "Description of laboratory 4";
 
             return View();
         }
@@ -29,10 +45,6 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
